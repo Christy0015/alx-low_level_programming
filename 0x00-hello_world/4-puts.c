4b10a4e8531760fa19a83c6c892c7eec17767e2a
Christiana <christianaokegbile@gmail.com>
2023-04-20 14:08:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - Entry point
  * Return: 0 (Sucess)
  */
-int main(void) 
-{ 
-puts ("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-} 
+}
